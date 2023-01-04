4043f0299369dab32d9a7dd5b31cf04c3cdb1581
Graham White <gwhite@uk.ibm.com>
2023-01-04 10:26:25 +0000

    Update the response type to longtext to cope with longer responses from OSM

--- mod_osm/sql/install.mysql.utf8.sql
+++ mod_osm/sql/install.mysql.utf8.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `#__mod_osm` (
 	`url` VARCHAR(191) NOT NULL,
 	`fetched_at` DATETIME NOT NULL,
-	`response` text,
+	`response` longtext,
   PRIMARY KEY (`url`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
